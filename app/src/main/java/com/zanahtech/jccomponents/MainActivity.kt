3cbaf90970b32f33a13f8cb2ae31d4383f5efeec
kiprono <im.kiprono@gmail.com>
2024-02-14 08:16:36 +0300

    layouts-rbc

--- app/src/main/java/com/zanahtech/jccomponents/MainActivity.kt
+++ app/src/main/java/com/zanahtech/jccomponents/MainActivity.kt
@@ -5,9 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -32,9 +37,10 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Box(
                     modifier = Modifier.fillMaxSize(),
-                    contentAlignment = Alignment.Center
+                    contentAlignment = Alignment.CenterStart
                 ) {
                     LearnTextAndModifiers()
+                    
                 }
             }
         }
@@ -45,23 +51,63 @@ class MainActivity : ComponentActivity() {
 fun LearnTextAndModifiers(){
 
     val clickOnText = {}
-    Text(text = stringResource(id = R.string.hello_text),
-        color = Color.Blue,
-        fontSize = 32.sp,
-        fontStyle = FontStyle.Italic,
-        modifier = Modifier
-            .padding(22.dp)
-            .background(Color.Black)
-            .clickable(onClick = clickOnText)
-    )
-}
+    
+    Column(horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
 
-@Preview(showBackground = true)
-@Composable
-fun  ShowPreview(){
-    JccomponentsTheme {
-        LearnTextAndModifiers()
-    }
+//    Text(text = stringResource(id = R.string.hello_text),
+//        color = Color.Blue,
+//        fontSize = 32.sp,
+//        fontStyle = FontStyle.Italic,
+//        modifier = Modifier
+//            .padding(22.dp)
+//            .background(Color.Black)
+//            .clickable(onClick = clickOnText)
+//    )
+//        Text(text = "Hello Column 1")
+//        Text(text = "Hello Column 2")
+//}
+//
+//    Row (verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.Center) {
+//        Text(text = stringResource(id = R.string.hello_text),
+//            color = Color.Blue,
+//            fontSize = 32.sp,
+//            fontStyle = FontStyle.Italic,
+//            modifier = Modifier
+//                .padding(22.dp)
+//                .background(Color.Black)
+//                .clickable(onClick = clickOnText)
+//        )
+//        Text(text = "Hello Column 1")
+//        Text(text = "Hello Column 2")
+//
+//    }
+
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Gray), contentAlignment = Alignment.Center){
+            Box(modifier = Modifier.height(300.dp).width(300.dp).background(Color.Blue) ){
+
+                Text(text = stringResource(id = R.string.hello_text),
+                    color = Color.White,
+                    fontSize = 30.sp,
+                    fontStyle = FontStyle.Italic,
+                    modifier = Modifier
+                        .align(Alignment.Center)
+                        .padding(22.dp)
+                        .background(Color.Black)
+                        .clickable(onClick = clickOnText)
+                )
+
+
+            }
+
+
+
+
+            }
+        }
+
+
 
 
 }
