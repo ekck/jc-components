2fc04eb9ad879720f90fb4796880fbe22564570c
kiprono <im.kiprono@gmail.com>
2024-02-14 09:34:14 +0300

    alignmentandarrangement

--- app/src/main/java/com/zanahtech/jccomponents/MainActivity.kt
+++ app/src/main/java/com/zanahtech/jccomponents/MainActivity.kt
@@ -35,11 +35,12 @@ class MainActivity : ComponentActivity() {
         setContent {
             JccomponentsTheme {
                 // A surface container using the 'background' color from the theme
-                Box(
+                Surface(
                     modifier = Modifier.fillMaxSize(),
-                    contentAlignment = Alignment.CenterStart
+                    color = MaterialTheme.colorScheme.background
+//                    contentAlignment = Alignment.CenterStart
                 ) {
-                    LearnTextAndModifiers()
+                    LearnAlignmentArrangement()
                     
                 }
             }
@@ -47,69 +48,96 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun LearnTextAndModifiers(){
-
-    val clickOnText = {}
-    
-    Column(horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
-
-//    Text(text = stringResource(id = R.string.hello_text),
-//        color = Color.Blue,
-//        fontSize = 32.sp,
-//        fontStyle = FontStyle.Italic,
-//        modifier = Modifier
-//            .padding(22.dp)
-//            .background(Color.Black)
-//            .clickable(onClick = clickOnText)
-//    )
-//        Text(text = "Hello Column 1")
-//        Text(text = "Hello Column 2")
+//@Composable
+//fun LearnTextAndModifiers(){
+//
+//    val clickOnText = {}
+//
+//    Column(horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
+//
+////    Text(text = stringResource(id = R.string.hello_text),
+////        color = Color.Blue,
+////        fontSize = 32.sp,
+////        fontStyle = FontStyle.Italic,
+////        modifier = Modifier
+////            .padding(22.dp)
+////            .background(Color.Black)
+////            .clickable(onClick = clickOnText)
+////    )
+////        Text(text = "Hello Column 1")
+////        Text(text = "Hello Column 2")
+////}
+////
+////    Row (verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.Center) {
+////        Text(text = stringResource(id = R.string.hello_text),
+////            color = Color.Blue,
+////            fontSize = 32.sp,
+////            fontStyle = FontStyle.Italic,
+////            modifier = Modifier
+////                .padding(22.dp)
+////                .background(Color.Black)
+////                .clickable(onClick = clickOnText)
+////        )
+////        Text(text = "Hello Column 1")
+////        Text(text = "Hello Column 2")
+////
+////    }
+//
+//        Box(modifier = Modifier
+//            .fillMaxSize()
+//            .background(Color.Gray), contentAlignment = Alignment.Center){
+//            Box(modifier = Modifier.height(300.dp).width(300.dp).background(Color.Blue) ){
+//
+//                Text(text = stringResource(id = R.string.hello_text),
+//                    color = Color.White,
+//                    fontSize = 30.sp,
+//                    fontStyle = FontStyle.Italic,
+//                    modifier = Modifier
+//                        .align(Alignment.Center)
+//                        .padding(22.dp)
+//                        .background(Color.Black)
+//                        .clickable(onClick = clickOnText)
+//                )
+//
+//
+//            }
+//
+//
+//
+//
+//            }
+//        }
+//
+//
+//
+//
 //}
-//
-//    Row (verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.Center) {
-//        Text(text = stringResource(id = R.string.hello_text),
-//            color = Color.Blue,
-//            fontSize = 32.sp,
-//            fontStyle = FontStyle.Italic,
-//            modifier = Modifier
-//                .padding(22.dp)
-//                .background(Color.Black)
-//                .clickable(onClick = clickOnText)
-//        )
-//        Text(text = "Hello Column 1")
-//        Text(text = "Hello Column 2")
-//
+
+//Alignment = Cross Axis (Row = Vertical, Column = Horizontal)
+//Arrangement = Main Axis (Row = Horizontal, Column = Vertical)
+
+@Composable
+fun LearnAlignmentArrangement(){
+
+    //RowAlignment: Top, CenterVertically, Bottom
+    //RowArrangement: Start, Center, End, SpaceBetween, SpaceAround, SpaceEvenly,
+            //Absolute.Left, Absolute.Right, Absolute.Center
+            //Absolute.SpaceBetween, Absolute.SpaceAround, Absolute.SpaceEvenly
+//    Row(verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.Center){
+//        Text(text = "Row Alignment Arrangement")
 //    }
 
-        Box(modifier = Modifier
-            .fillMaxSize()
-            .background(Color.Gray), contentAlignment = Alignment.Center){
-            Box(modifier = Modifier.height(300.dp).width(300.dp).background(Color.Blue) ){
+   Box(contentAlignment = Alignment.BottomCenter){
+       Text(text = "text", modifier = Modifier.align(Alignment.Center))
+       Text(text = "text 2", modifier = Modifier.align(Alignment.TopCenter))
+       Text(text = "text3", modifier = Modifier.align(Alignment.TopEnd))
+       Text(text = "text4", modifier = Modifier.align(Alignment.TopStart))
+       Text(text = "text5", modifier = Modifier.align(Alignment.BottomEnd))
+       Text(text = "text6", modifier = Modifier.align(Alignment.CenterStart))
+       Text(text = "text8", modifier = Modifier.align(Alignment.CenterEnd))
+       Text(text = "text9", modifier = Modifier.align(Alignment.CenterStart))
 
-                Text(text = stringResource(id = R.string.hello_text),
-                    color = Color.White,
-                    fontSize = 30.sp,
-                    fontStyle = FontStyle.Italic,
-                    modifier = Modifier
-                        .align(Alignment.Center)
-                        .padding(22.dp)
-                        .background(Color.Black)
-                        .clickable(onClick = clickOnText)
-                )
-
-
-            }
-
-
-
-
-            }
-        }
-
-
-
-
+   }   
 }
 
 
