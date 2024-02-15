5bfb790d5254a846a332184bdc8588a15b011aa2
kiprono <im.kiprono@gmail.com>
2024-02-15 11:21:17 +0300

    NavigationDrawer

--- app/src/main/java/com/zanahtech/jccomponents/MainActivity.kt
+++ app/src/main/java/com/zanahtech/jccomponents/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.zanahtech.jccomponents
 
+import android.annotation.SuppressLint
+import android.graphics.drawable.Icon
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
@@ -13,28 +15,42 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.ExitToApp
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.rounded.Menu
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -47,8 +63,12 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.zanahtech.jccomponents.ui.theme.GreenJC
 import com.zanahtech.jccomponents.ui.theme.JccomponentsTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -62,8 +82,9 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
 //                    contentAlignment = Alignment.CenterStart
                 ) {
-                    LearnTopAppBar()
+//                    LearnTopAppBar()
 //                    LearnState()
+                    LearnNavDrawer()
                     
                 }
             }
@@ -163,96 +184,208 @@ class MainActivity : ComponentActivity() {
 //   }
 //}
 
+//
+//
+//@Composable
+//fun LearnButton(){
+//
+//    val context = LocalContext.current.applicationContext
+//
+//    Column (verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+//        Button(onClick = { Toast.makeText(context, "Login successful", Toast.LENGTH_SHORT).show()},
+//
+//            shape = RoundedCornerShape(size = 16.dp), colors = ButtonDefaults.buttonColors(containerColor = Color.Cyan)
+//        ) {
+//            Text(text = "Login")
+//
+//        }
+//    }
+//}
+//
+//@Composable
+//fun LearnImage(){
+//    val context = LocalContext.current.applicationContext
+//
+//    Column (verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+//        Image(painter = painterResource(id = R.drawable.work), contentDescription = "work in progress" )
+//
+//        Button(onClick = { Toast.makeText(context, "Login successful", Toast.LENGTH_SHORT).show()},
+//
+//            shape = RoundedCornerShape(size = 16.dp), colors = ButtonDefaults.buttonColors(containerColor = Color.Cyan)
+//        ) {
+//            Text(text = "Login")
+//
+//        }
+//
+//    }
+//
+//}
+//
+//@Composable
+//fun LearnState(){
+////    var age = 0
+//    var age by remember {
+//        mutableStateOf(0)
+//    }
+//
+//
+//    Column (verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally){
+//        Button(onClick = { age++
+//        Log.v("TAG", "age"+age)}) {
+//            Text(text = "I am $age years old")
+//
+//
+//        }
+//    }
+//}
+//
+//@OptIn(ExperimentalMaterial3Api::class)
+//@Composable
+//fun LearnTopAppBar(){
+//
+//    val context = LocalContext.current.applicationContext
+//    TopAppBar(title = { Text(text = "WhatsApp")},
+//        navigationIcon = {
+//            IconButton(onClick = {Toast.makeText(context, "Whatsapp", Toast.LENGTH_SHORT).show()}) {
+//                Icon(painter = painterResource(id = R.drawable.splash), contentDescription = "whatsapp icon")
+//
+//            }
+//        }, colors = TopAppBarDefaults.topAppBarColors(
+//            containerColor = GreenJC,
+//            titleContentColor = Color.White,
+//            navigationIconContentColor = Color.White
+//        ), actions = {
+//            IconButton(onClick = { Toast.makeText(context, "Profile", Toast.LENGTH_SHORT).show() }) {
+//                Icon(imageVector = Icons.Filled.Person, contentDescription = "Profile", tint= Color.White)
+//
+//            }
+//            IconButton(onClick = { Toast.makeText(context, "Search", Toast.LENGTH_SHORT).show() }) {
+//                Icon(imageVector = Icons.Filled.Search, contentDescription = "Profile", tint= Color.White)
+//
+//            }
+//            IconButton(onClick = { Toast.makeText(context, "Menu", Toast.LENGTH_SHORT).show() }) {
+//                Icon(imageVector = Icons.Filled.MoreVert, contentDescription = "Profile", tint= Color.White)
+//
+//            }
+//        }
+//        )
+//
+//}
+//
+//@Preview
+//@Composable
+//fun TopAppBarPreview(){
+//    LearnTopAppBar()
+//}
 
-
-@Composable
-fun LearnButton(){
-
-    val context = LocalContext.current.applicationContext
-
-    Column (verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
-        Button(onClick = { Toast.makeText(context, "Login successful", Toast.LENGTH_SHORT).show()},
-
-            shape = RoundedCornerShape(size = 16.dp), colors = ButtonDefaults.buttonColors(containerColor = Color.Cyan)
-        ) {
-            Text(text = "Login")
-
-        }
-    }
-}
-
-@Composable
-fun LearnImage(){
-    val context = LocalContext.current.applicationContext
-
-    Column (verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
-        Image(painter = painterResource(id = R.drawable.work), contentDescription = "work in progress" )
-
-        Button(onClick = { Toast.makeText(context, "Login successful", Toast.LENGTH_SHORT).show()},
-
-            shape = RoundedCornerShape(size = 16.dp), colors = ButtonDefaults.buttonColors(containerColor = Color.Cyan)
-        ) {
-            Text(text = "Login")
-
-        }
-
-    }
-
-}
-
-@Composable
-fun LearnState(){
-//    var age = 0
-    var age by remember {
-        mutableStateOf(0)
-    }
-
-
-    Column (verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally){
-        Button(onClick = { age++
-        Log.v("TAG", "age"+age)}) {
-            Text(text = "I am $age years old")
-
-            
-        }
-    }
-}
-
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun LearnTopAppBar(){
-
+fun LearnNavDrawer(){
+    val navigationController = rememberNavController()
+    val coroutineScope = rememberCoroutineScope()
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
     val context = LocalContext.current.applicationContext
-    TopAppBar(title = { Text(text = "WhatsApp")},
-        navigationIcon = {
-            IconButton(onClick = {Toast.makeText(context, "Whatsapp", Toast.LENGTH_SHORT).show()}) {
-                Icon(painter = painterResource(id = R.drawable.splash), contentDescription = "whatsapp icon")
+    
+    ModalNavigationDrawer(
+         drawerState = drawerState,
+        gesturesEnabled = true,
+        drawerContent = { 
+            ModalDrawerSheet {
+                Box(modifier = Modifier
+                    .background(GreenJC)
+                    .fillMaxWidth()
+                    .height(150.dp))
+                { 
+                    Text(text = "")
+                }
+                
+                Divider()
+                NavigationDrawerItem(label = { Text(text = "Home", color = GreenJC) },
+                    selected = false,
+                    icon = { Icon(imageVector = Icons.Default.Home, contentDescription = "Home", tint= GreenJC)},
+                    onClick = {
+                        coroutineScope.launch {
+                            drawerState.close()
+                        }
+                        navigationController.navigate(Screens.Home.screen){
+                            popUpTo(0)
+                        }
+                    })
+
+                NavigationDrawerItem(label = { Text(text = "Profile", color = GreenJC) },
+                    selected = false,
+                    icon = { Icon(imageVector = Icons.Default.AccountBox, contentDescription = "Profile", tint= GreenJC)},
+                    onClick = {
+                        coroutineScope.launch {
+                            drawerState.close()
+                        }
+                        navigationController.navigate(Screens.Profile.screen){
+                            popUpTo(0)
+                        }
+                    })
+
+
+                NavigationDrawerItem(label = { Text(text = "Settings", color = GreenJC) },
+                    selected = false,
+                    icon = { Icon(imageVector = Icons.Default.Settings, contentDescription = "Settings", tint= GreenJC)},
+                    onClick = {
+                        coroutineScope.launch {
+                            drawerState.close()
+                        }
+                        navigationController.navigate(Screens.Settings.screen){
+                            popUpTo(0)
+                        }
+                    })
+
+                NavigationDrawerItem(label = { Text(text = "Logout", color = GreenJC) },
+                    selected = false,
+                    icon = { Icon(imageVector = Icons.Default.ExitToApp, contentDescription = "Logout", tint= GreenJC)},
+                    onClick = {
+                        coroutineScope.launch {
+                            drawerState.close()
+                        }
+                       Toast.makeText(context, "Logout", Toast.LENGTH_SHORT).show()
+                    })
+
                 
             }
-        }, colors = TopAppBarDefaults.topAppBarColors(
-            containerColor = GreenJC,
-            titleContentColor = Color.White,
-            navigationIconContentColor = Color.White
-        ), actions = {
-            IconButton(onClick = { Toast.makeText(context, "Profile", Toast.LENGTH_SHORT).show() }) {
-                Icon(imageVector = Icons.Filled.Person, contentDescription = "Profile", tint= Color.White)
-                
-            }
-            IconButton(onClick = { Toast.makeText(context, "Search", Toast.LENGTH_SHORT).show() }) {
-                Icon(imageVector = Icons.Filled.Search, contentDescription = "Profile", tint= Color.White)
+        }) {
+        Scaffold (
+            topBar = {
+                val coroutineScope = rememberCoroutineScope()
+                TopAppBar(title = { Text(text = "WhatsApp")},
+                    colors = TopAppBarDefaults.topAppBarColors(
+                        containerColor = GreenJC,
+                        titleContentColor = Color.White,
+                        navigationIconContentColor = Color.White
+                    ),
+                    navigationIcon = {
+                        IconButton(onClick = {
+                            coroutineScope.launch {
+                                drawerState.open()
+                            }
+                        }) {
+                            Icon(
+                                Icons.Rounded.Menu, contentDescription = "MenuButton"
+                            )
+                        }
+                    },
+                    )
 
             }
-            IconButton(onClick = { Toast.makeText(context, "Menu", Toast.LENGTH_SHORT).show() }) {
-                Icon(imageVector = Icons.Filled.MoreVert, contentDescription = "Profile", tint= Color.White)
+        ) {
+            NavHost(navController = navigationController,
+                startDestination = Screens.Home.screen ){
+                composable(Screens.Home.screen){ Home() }
+                composable(Screens.Profile.screen){ Profile() }
+                composable(Screens.Settings.screen){ Settings() }
+
 
             }
+
         }
-        )
 
-}
-
-@Preview
-@Composable
-fun TopAppBarPreview(){
-    LearnTopAppBar()
+        
+    }
 }
---
+++ app/src/main/java/com/zanahtech/jccomponents/Screens.kt
@@ -0,0 +1,7 @@
+package com.zanahtech.jccomponents
+
+sealed class Screens (val screen: String){
+    data object Home: Screens("home")
+    data object Profile: Screens("profile")
+    data object Settings: Screens("settings")
+}
